8ab4cd6120be5b580f6d1fa32cf2e069a5c63c44
wenjingw <wenjingw@whatsapp.com>
2021-03-28 23:15:52 -0700

    [leetcode] tree 236.lowest-common-ancestor-of-a-binary-tree.py

---
+++ 236.lowest-common-ancestor-of-a-binary-tree.py
@@ -0,0 +1,96 @@
+#
+# @lc app=leetcode id=236 lang=python3
+#
+# [236] Lowest Common Ancestor of a Binary Tree
+#
+# https://leetcode.com/problems/lowest-common-ancestor-of-a-binary-tree/description/
+#
+# algorithms
+# Medium (48.70%)
+# Likes:    5410
+# Dislikes: 201
+# Total Accepted:    622.7K
+# Total Submissions: 1.3M
+# Testcase Example:  '[3,5,1,6,2,0,8,null,null,7,4]\n5\n1'
+#
+# Given a binary tree, find the lowest common ancestor (LCA) of two given nodes
+# in the tree.
+# 
+# According to the definition of LCA on Wikipedia: “The lowest common ancestor
+# is defined between two nodes p and q as the lowest node in T that has both p
+# and q as descendants (where we allow a node to be a descendant of
+# itself).”
+# 
+# 
+# Example 1:
+# 
+# 
+# Input: root = [3,5,1,6,2,0,8,null,null,7,4], p = 5, q = 1
+# Output: 3
+# Explanation: The LCA of nodes 5 and 1 is 3.
+# 
+# 
+# Example 2:
+# 
+# 
+# Input: root = [3,5,1,6,2,0,8,null,null,7,4], p = 5, q = 4
+# Output: 5
+# Explanation: The LCA of nodes 5 and 4 is 5, since a node can be a descendant
+# of itself according to the LCA definition.
+# 
+# 
+# Example 3:
+# 
+# 
+# Input: root = [1,2], p = 1, q = 2
+# Output: 1
+# 
+# 
+# 
+# Constraints:
+# 
+# 
+# The number of nodes in the tree is in the range [2, 10^5].
+# -10^9 <= Node.val <= 10^9
+# All Node.val are unique.
+# p != q
+# p and q will exist in the tree.
+# 
+# 
+#
+from collections import defaultdict
+# @lc code=start
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+
+class Solution:
+    def lowestCommonAncestor(self, root: TreeNode, p: TreeNode, q: TreeNode) -> TreeNode:
+        parent = {root: None}
+        stack = [root]
+        while p not in parent or q not in parent:
+            node = stack.pop()
+            if node.left:
+                parent[node.left] = node
+                stack.append(node.left)
+            if node.right:
+                parent[node.right] = node
+                stack.append(node.right)
+
+        path = defaultdict(bool)
+        node = p
+        while node:
+            path[node] = True
+            node = parent[node]
+
+        node = q
+        while node and node not in path:
+            node = parent[node]
+
+        return node
+
+# @lc code=end
