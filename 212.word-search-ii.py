57f677317df628e375112f16d9888f17a55dbcab
wenjingw <wenjingw@whatsapp.com>
2021-04-07 23:25:15 -0700

    [leetcode] Trie + backtracking 212.word-search-ii.py

--- 212.word-search-ii.py
+++ 212.word-search-ii.py
@@ -58,60 +58,82 @@ from typing import List
 
 # @lc code=start
 
+END_OF_WORD = '$'
+
 
 class Solution:
+
     def findWords(self, board: List[List[str]], words: List[str]) -> List[str]:
-        start_points = defaultdict(list)
-        m = len(board)
-        n = len(board[0])
-        for i in range(m):
-            for j in range(n):
-                cells = start_points[board[i][j]]
-                cells.append((i, j))
-        visited = [[False for j in range(n)]for i in range(m)]
-        words_set = set()
-        for word in words:
-            for [x, y] in start_points[word[0]]:
-                if self.findWord(board, visited, word, 0, x, y):
-                    words_set.add(word)
-        return list(words_set)
+        trie = self.__buildWordTrie(words)
+        # print(trie)
+        row_num = len(board)
+        col_num = len(board[0])
 
-    def findWord(self, board: List[List[str]], visited: List[List[bool]], word: str, idx: int, x: int, y: int) -> bool:
-        if word[idx] != board[x][y]:
-            return False
-        elif idx == len(word) - 1:
-            return True 
+        visited = [[False for j in range(col_num)]for i in range(row_num)]
+        words_matched = []
+        for row in range(row_num):
+            for col in range(col_num):
+                if board[row][col] in trie:
+                    words_matched += self.__backtrack(
+                        board, visited, row, col, trie)
 
-        visited[x][y] = True
-        idx += 1
+        return words_matched
 
-        found = False
-        for [dx, dy] in [(0, 1), (0, -1), (1, 0), (-1, 0)]:
-            new_x = x + dx
-            new_y = y + dy
-            if new_x < 0 or new_x >= len(board) or new_y < 0 or new_y >= len(board[0]):
+    def __buildWordTrie(self, words: List[str]) -> dict:
+        root_node = {}
+        for w in words:
+            current_node = root_node
+            for letter in w:
+                current_node = current_node.setdefault(letter, {})
+            current_node[END_OF_WORD] = w
+        return root_node
+
+    def __backtrack(self, board: List[List[str]], visited: List[List[str]], row: int, col: int, trie_node: dict) -> List[str]:
+        # print(board[row][col], row, col)
+        letter = board[row][col]
+        curr_node = trie_node[letter]
+        words_matched = []
+        if END_OF_WORD in curr_node:
+            # print('matched', curr_node[END_OF_WORD])
+            words_matched.append(curr_node[END_OF_WORD])
+            curr_node.pop(END_OF_WORD, False)
+
+        row_num = len(board)
+        col_num = len(board[0])
+        visited[row][col] = True
+        for (row_offset, col_offset) in [(1, 0), (-1, 0), (0, 1), (0, -1)]:
+            new_row, new_col = row + row_offset, col + col_offset
+            if new_row < 0 or new_row >= row_num or new_col < 0 or new_col >= col_num:
                 continue
-            elif visited[new_x][new_y]:
+            elif visited[new_row][new_col]:
                 continue
-            elif self.findWord(board, visited, word, idx, new_x, new_y):
-                found = True
+            elif board[new_row][new_col] not in curr_node:
+                continue
+            else:
+                words_matched += self.__backtrack(board,
+                                                  visited, new_row, new_col, curr_node)
 
-        visited[x][y] = False
-        return found
+        visited[row][col] = False
+        # print(words_matched)
+        return words_matched
 
 
 # @lc code=end
 
-s = Solution()
-board = [["a","b"],["c","d"]]
-words = ["abcb"]
-board = [["a"]]
-words = ["a"]
-board = [["a","b"],["c","d"]]
-words = ["abcb"]
-board = [["o", "a", "a", "n"], ["e", "t", "a", "e"],
-         ["i", "h", "k", "r"], ["i", "f", "l", "v"]]
-words = ["oath", "pea", "eat", "rain"]
-board = [["o","a","b","n"],["o","t","a","e"],["a","h","k","r"],["a","f","l","v"]]
-words = ["oa","oaa"]
-print(s.findWords(board, words))
+# s = Solution()
+# board = [["a", "b"], ["c", "d"]]
+# words = ["abcb"]
+# board = [["a"]]
+# words = ["a"]
+# board = [["o", "a", "b", "n"], ["o", "t", "a", "e"],
+        #  ["a", "h", "k", "r"], ["a", "f", "l", "v"]]
+# words = ["oa", "oaa"]
+# board = [["a", "b"], ["c", "d"]]
+# words = ["abcb"]
+# board = [["o", "a", "a", "n"], ["e", "t", "a", "e"],
+        #  ["i", "h", "k", "r"], ["i", "f", "l", "v"]]
+# words = ["oath", "pea", "eat", "rain"]
+# board = [["b","a","b","a","b","a","b","a","b","a"],["a","b","a","b","a","b","a","b","a","b"],["b","a","b","a","b","a","b","a","b","a"],["a","b","a","b","a","b","a","b","a","b"],["b","a","b","a","b","a","b","a","b","a"],["a","b","a","b","a","b","a","b","a","b"],["b","a","b","a","b","a","b","a","b","a"],["a","b","a","b","a","b","a","b","a","b"],["b","a","b","a","b","a","b","a","b","a"],["a","b","a","b","a","b","a","b","a","b"]]
+# words = ["ababababaa","ababababab","ababababac","ababababad","ababababae","ababababaf","ababababag","ababababah","ababababai","ababababaj","ababababak","ababababal","ababababam","ababababan","ababababao","ababababap","ababababaq","ababababar","ababababas","ababababat","ababababau","ababababav","ababababaw","ababababax","ababababay","ababababaz","ababababba","ababababbb","ababababbc","ababababbd","ababababbe","ababababbf","ababababbg","ababababbh","ababababbi","ababababbj","ababababbk","ababababbl","ababababbm","ababababbn","ababababbo","ababababbp","ababababbq","ababababbr","ababababbs","ababababbt","ababababbu","ababababbv","ababababbw","ababababbx","ababababby","ababababbz","ababababca","ababababcb","ababababcc","ababababcd","ababababce","ababababcf","ababababcg","ababababch","ababababci","ababababcj","ababababck","ababababcl","ababababcm","ababababcn","ababababco","ababababcp","ababababcq","ababababcr","ababababcs","ababababct","ababababcu","ababababcv","ababababcw","ababababcx","ababababcy","ababababcz","ababababda","ababababdb","ababababdc","ababababdd","ababababde","ababababdf","ababababdg","ababababdh","ababababdi","ababababdj","ababababdk","ababababdl","ababababdm","ababababdn","ababababdo","ababababdp","ababababdq","ababababdr","ababababds","ababababdt","ababababdu","ababababdv","ababababdw","ababababdx","ababababdy","ababababdz","ababababea","ababababeb","ababababec","ababababed","ababababee","ababababef","ababababeg","ababababeh","ababababei","ababababej","ababababek","ababababel","ababababem","ababababen","ababababeo","ababababep","ababababeq","ababababer","ababababes","ababababet","ababababeu","ababababev","ababababew","ababababex","ababababey","ababababez","ababababfa","ababababfb","ababababfc","ababababfd","ababababfe","ababababff","ababababfg","ababababfh","ababababfi","ababababfj","ababababfk","ababababfl","ababababfm","ababababfn","ababababfo","ababababfp","ababababfq","ababababfr","ababababfs","ababababft","ababababfu","ababababfv","ababababfw","ababababfx","ababababfy","ababababfz","ababababga","ababababgb","ababababgc","ababababgd","ababababge","ababababgf","ababababgg","ababababgh","ababababgi","ababababgj","ababababgk","ababababgl","ababababgm","ababababgn","ababababgo","ababababgp","ababababgq","ababababgr","ababababgs","ababababgt","ababababgu","ababababgv","ababababgw","ababababgx","ababababgy","ababababgz","ababababha","ababababhb","ababababhc","ababababhd","ababababhe","ababababhf","ababababhg","ababababhh","ababababhi","ababababhj","ababababhk","ababababhl","ababababhm","ababababhn","ababababho","ababababhp","ababababhq","ababababhr","ababababhs","ababababht","ababababhu","ababababhv","ababababhw","ababababhx","ababababhy","ababababhz","ababababia","ababababib","ababababic","ababababid","ababababie","ababababif","ababababig","ababababih","ababababii","ababababij","ababababik","ababababil","ababababim","ababababin","ababababio","ababababip","ababababiq","ababababir","ababababis","ababababit","ababababiu","ababababiv","ababababiw","ababababix","ababababiy","ababababiz","ababababja","ababababjb","ababababjc","ababababjd","ababababje","ababababjf","ababababjg","ababababjh","ababababji","ababababjj","ababababjk","ababababjl","ababababjm","ababababjn","ababababjo","ababababjp","ababababjq","ababababjr","ababababjs","ababababjt","ababababju","ababababjv","ababababjw","ababababjx","ababababjy","ababababjz","ababababka","ababababkb","ababababkc","ababababkd","ababababke","ababababkf","ababababkg","ababababkh","ababababki","ababababkj","ababababkk","ababababkl","ababababkm","ababababkn","ababababko","ababababkp","ababababkq","ababababkr","ababababks","ababababkt","ababababku","ababababkv","ababababkw","ababababkx","ababababky","ababababkz","ababababla","abababablb","abababablc","ababababld","abababable","abababablf","abababablg","abababablh","ababababli","abababablj","abababablk","ababababll","abababablm","ababababln","abababablo","abababablp","abababablq","abababablr","ababababls","abababablt","abababablu","abababablv","abababablw","abababablx","abababably","abababablz","ababababma","ababababmb","ababababmc","ababababmd","ababababme","ababababmf","ababababmg","ababababmh","ababababmi","ababababmj","ababababmk","ababababml","ababababmm","ababababmn","ababababmo","ababababmp","ababababmq","ababababmr","ababababms","ababababmt","ababababmu","ababababmv","ababababmw","ababababmx","ababababmy","ababababmz","ababababna","ababababnb","ababababnc","ababababnd","ababababne","ababababnf","ababababng","ababababnh","ababababni","ababababnj","ababababnk","ababababnl","ababababnm","ababababnn","ababababno","ababababnp","ababababnq","ababababnr","ababababns","ababababnt","ababababnu","ababababnv","ababababnw","ababababnx","ababababny","ababababnz","ababababoa","ababababob","ababababoc","ababababod","ababababoe","ababababof","ababababog","ababababoh","ababababoi","ababababoj","ababababok","ababababol","ababababom","ababababon","ababababoo","ababababop","ababababoq","ababababor","ababababos","ababababot","ababababou","ababababov","ababababow","ababababox","ababababoy","ababababoz","ababababpa","ababababpb","ababababpc","ababababpd","ababababpe","ababababpf","ababababpg","ababababph","ababababpi","ababababpj","ababababpk","ababababpl","ababababpm","ababababpn","ababababpo","ababababpp","ababababpq","ababababpr","ababababps","ababababpt","ababababpu","ababababpv","ababababpw","ababababpx","ababababpy","ababababpz","ababababqa","ababababqb","ababababqc","ababababqd","ababababqe","ababababqf","ababababqg","ababababqh","ababababqi","ababababqj","ababababqk","ababababql","ababababqm","ababababqn","ababababqo","ababababqp","ababababqq","ababababqr","ababababqs","ababababqt","ababababqu","ababababqv","ababababqw","ababababqx","ababababqy","ababababqz","ababababra","ababababrb","ababababrc","ababababrd","ababababre","ababababrf","ababababrg","ababababrh","ababababri","ababababrj","ababababrk","ababababrl","ababababrm","ababababrn","ababababro","ababababrp","ababababrq","ababababrr","ababababrs","ababababrt","ababababru","ababababrv","ababababrw","ababababrx","ababababry","ababababrz","ababababsa","ababababsb","ababababsc","ababababsd","ababababse","ababababsf","ababababsg","ababababsh","ababababsi","ababababsj","ababababsk","ababababsl","ababababsm","ababababsn","ababababso","ababababsp","ababababsq","ababababsr","ababababss","ababababst","ababababsu","ababababsv","ababababsw","ababababsx","ababababsy","ababababsz","ababababta","ababababtb","ababababtc","ababababtd","ababababte","ababababtf","ababababtg","ababababth","ababababti","ababababtj","ababababtk","ababababtl","ababababtm","ababababtn","ababababto","ababababtp","ababababtq","ababababtr","ababababts","ababababtt","ababababtu","ababababtv","ababababtw","ababababtx","ababababty","ababababtz","ababababua","ababababub","ababababuc","ababababud","ababababue","ababababuf","ababababug","ababababuh","ababababui","ababababuj","ababababuk","ababababul","ababababum","ababababun","ababababuo","ababababup","ababababuq","ababababur","ababababus","ababababut","ababababuu","ababababuv","ababababuw","ababababux","ababababuy","ababababuz","ababababva","ababababvb","ababababvc","ababababvd","ababababve","ababababvf","ababababvg","ababababvh","ababababvi","ababababvj","ababababvk","ababababvl","ababababvm","ababababvn","ababababvo","ababababvp","ababababvq","ababababvr","ababababvs","ababababvt","ababababvu","ababababvv","ababababvw","ababababvx","ababababvy","ababababvz","ababababwa","ababababwb","ababababwc","ababababwd","ababababwe","ababababwf","ababababwg","ababababwh","ababababwi","ababababwj","ababababwk","ababababwl","ababababwm","ababababwn","ababababwo","ababababwp","ababababwq","ababababwr","ababababws","ababababwt","ababababwu","ababababwv","ababababww","ababababwx","ababababwy","ababababwz","ababababxa","ababababxb","ababababxc","ababababxd","ababababxe","ababababxf","ababababxg","ababababxh","ababababxi","ababababxj","ababababxk","ababababxl","ababababxm","ababababxn","ababababxo","ababababxp","ababababxq","ababababxr","ababababxs","ababababxt","ababababxu","ababababxv","ababababxw","ababababxx","ababababxy","ababababxz","ababababya","ababababyb","ababababyc","ababababyd","ababababye","ababababyf","ababababyg","ababababyh","ababababyi","ababababyj","ababababyk","ababababyl","ababababym","ababababyn","ababababyo","ababababyp","ababababyq","ababababyr","ababababys","ababababyt","ababababyu","ababababyv","ababababyw","ababababyx","ababababyy","ababababyz","ababababza","ababababzb","ababababzc","ababababzd","ababababze","ababababzf","ababababzg","ababababzh","ababababzi","ababababzj","ababababzk","ababababzl","ababababzm","ababababzn","ababababzo","ababababzp","ababababzq","ababababzr","ababababzs","ababababzt","ababababzu","ababababzv","ababababzw","ababababzx","ababababzy","ababababzz"]
+# print(s.findWords(board, words))
+# 
